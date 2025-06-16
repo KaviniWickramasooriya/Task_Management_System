15f65815e699a086781f2362f980c76af970a49f
Kavini Wickramasooriya <165365634+KaviniWickramasooriya@users.noreply.github.com>
2025-06-16 10:17:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 import express from 'express';
+import cors from 'cors';
+
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import otpRoutes from './routes/otpRoutes.js';
+
+
 
 dotenv.config();
 const app = express();
+
+
+app.use(cors()); // Allow all origins
+// OR to allow only frontend:
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true // If you’re using cookies/auth headers
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,10 +29,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/otp', otpRoutes);
+
 
 // DB + Server
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
 .then(() => {
   app.listen(process.env.PORT, () =>
